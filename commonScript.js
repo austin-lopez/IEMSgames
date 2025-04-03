51172cdfd71b1b2aeb2495d5ee6b2b1248076a31
austin-lopez <austinlopez2022@u.northwestern.edu>
2025-04-03 01:25:18 -0500

    Update commonScript.js

--- commonScript.js
+++ commonScript.js
@@ -13,24 +13,34 @@ var sheetURL = (string, sheet) =>
     "https://sheets.googleapis.com/v4/spreadsheets/" + sheet + "/values/" + string + "?key=" + sheetKey;
 
 $.get(sheetURL("DATA", dataSheetID), function (data) {
-    sheets = data.values.slice(1,2)[0];
-    sheets.shift();
-    scripts = data.values.slice(2,3)[0];
-    scripts.shift();
+    
+    try {
+        sheets = data.values.slice(1,2)[0];
+        sheets.shift();
+        scripts = data.values.slice(2,3)[0];
+        scripts.shift();
 
-    sheets.forEach((element, index) => {
+        sheets.forEach((element, index) => {
 
-        $.get(sheetURL("DATA", element), function (data) {
-            window.sheetTitles[index] += (data.values.slice(0, 1)[0][0]);
+            $.get(sheetURL("DATA", element), function (data) {
+                window.sheetTitles[index] += (data.values.slice(0, 1)[0][0]);
+            });
+
+            $.get(sheetURL("NETIDS", element), function (data) {
+                var netIdData = data.values.slice(1);
+                netIdData.forEach(element => window.netIdDatabase[element[0].toUpperCase()] = element[1]);
+                window.sheetsLoaded++;
+
+            });
         });
-
-        $.get(sheetURL("NETIDS", element), function (data) {
-            var netIdData = data.values.slice(1);
-            netIdData.forEach(element => window.netIdDatabase[element[0].toUpperCase()] = element[1]);
-            window.sheetsLoaded++;
-                
-        });
-    });
+    }
+    
+    catch (e) {
+        console.log(e);
+        alert("Error loading data. Please wait a moment and refresh the page.");
+    }
+    
+    
 });
 
 
